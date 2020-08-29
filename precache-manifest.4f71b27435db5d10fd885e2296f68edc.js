f97d49c71d870e93c901ee407a85647d02e73552
nbr41to <nbr.41to@gmail.com>
2020-08-29 22:21:53 +0900

    Updates

---
+++ precache-manifest.4f71b27435db5d10fd885e2296f68edc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d384dc53db88aa99e711f40e4ea81dfe",
+    "url": "/react-second-app/index.html"
+  },
+  {
+    "revision": "9de549ffe93a8854bde5",
+    "url": "/react-second-app/static/js/2.bbfaf872.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/react-second-app/static/js/2.bbfaf872.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58316b01597f10d87777",
+    "url": "/react-second-app/static/js/main.70bf09aa.chunk.js"
+  },
+  {
+    "revision": "dd348b409ccbb7bcf9af",
+    "url": "/react-second-app/static/js/runtime-main.25d0f184.js"
+  }
+]);
